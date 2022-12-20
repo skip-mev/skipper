992629e31b4939cb75ddf2927a1752fadafbaad9
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2022-12-19 17:05:39 -0700

    Improve code separation

--- juno-arb/tx_parser.py
+++ juno-arb/tx_parser.py
@@ -14,12 +14,12 @@ def parse_tx(parser, tx, tx_bytes, message_value, msg) -> SingleSwap or PassThro
 def parse_junoswap(tx, tx_bytes, message_value, msg) -> SingleSwap or PassThroughSwap or None:
     if "swap" in msg:
         swap_tx = SingleSwap(tx=tx,
-                                tx_bytes=tx_bytes,
-                                sender=message_value.sender,
-                                contract_address=message_value.contract,
-                                input_token=msg['swap']['input_token'],
-                                input_amount=int(msg['swap']['input_amount']),
-                                min_output=int(msg['swap']['min_output']))
+                             tx_bytes=tx_bytes,
+                             sender=message_value.sender,
+                             contract_address=message_value.contract,
+                             input_token=msg['swap']['input_token'],
+                             input_amount=int(msg['swap']['input_amount']),
+                             min_output=int(msg['swap']['min_output']))
         return swap_tx
     elif "pass_through_swap" in msg:
         pass_through_swap_tx = PassThroughSwap(tx=tx,
