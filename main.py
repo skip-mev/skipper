0efc195bf560058324dfd6fa27351df3111a2f6d
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-01-06 00:50:31 -0500

    Separate bot running logic into own function

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
 import asyncio
 
-from src import Bot, Transaction 
+from src import Bot
+
 
 async def main():
-    # Create bot with the appropriate settings
+    """ Main function to run bot"""
     bot: Bot = Bot()
-    # Run bot in an infinite loop
-    while True:
-        # Update the account balance
-        bot.querier.update_account_balance(bot=bot) 
-        # Query the mempool for new transactions, returns once new txs are found
-        backrun_list = bot.querier.query_node_for_new_mempool_txs()
-        # Update the reserves of all pools when a new txs are found
-        await bot.state.update_all(jobs=bot.state.update_all_reserves_jobs)
-        # Iterate through each tx and assess for profitable opportunities
-        for tx_str in backrun_list:
-            # Create a transaction object
-            transaction: Transaction = Transaction(contracts=bot.state.contracts, 
-                                                   tx_str=tx_str, 
-                                                   decoder=bot.decoder,
-                                                   arb_denom=bot.arb_denom)
-            # Simulate the transaction on a copy of contract state 
-            # and return the copied state post-transaction simulation
-            contracts_copy = bot.state.simulate_transaction(transaction=transaction)
-            # Build the most profitable bundle from 
-            bundle: list = bot.build_most_profitable_bundle(transactions=transaction,
-                                                            contracts=contracts_copy)
-            # If there is a profitable bundle, fire away!
-            if bundle:
-                bot.fire(bundle=bundle)
+    await bot.run()
 
-# Printer go brrr
+
 if __name__ == "__main__":
     asyncio.run(main())
