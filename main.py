3f876e532ecd79719866feb268bd1e4b45bba933
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-01-11 13:51:42 -0500

    Move env file path switch to main

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import asyncio
 from src.bot import Bot 
 
 
+"""#############################################"""
+"""@USER TODO: CHOOSE ENVIRONMENT VARIABLES PATH"""
+ENV_FILE_PATH = "envs/juno.env"
+#ENV_FILE_PATH = "envs/terra.env"
+"""#############################################"""
+
+
 async def main():
     """ Main entry point to run bot."""
-    bot: Bot = Bot()
+    bot: Bot = Bot(env_file_path=ENV_FILE_PATH)
     await bot.init()
     await bot.run() 
 
