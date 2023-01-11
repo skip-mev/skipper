50a8b1c65962b08768d44dee7157f779c09dc218
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-01-11 13:04:23 -0500

    Make factory class a dataclass

--- src/contract/factory/factory.py
+++ src/contract/factory/factory.py
@@ -1,9 +1,11 @@
+from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
 from src.querier import Querier
 from src.contract import Contract
 
 
+@dataclass
 class Factory(Contract, ABC):
     """ This class is an abstract class for all factories."""
     protocol: str
