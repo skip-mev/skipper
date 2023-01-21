06900f0b386b179a600f2e587c8535c0ab1a9da4
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-01-21 18:29:59 -0500

    ✨ Add decoder support for evm txs

--- src/decoder/decoders/evm.py
+++ src/decoder/decoders/evm.py
@@ -4,17 +4,18 @@ from dataclasses import dataclass
 
 from cosmpy.protos.cosmos.tx.v1beta1.tx_pb2 import Tx
 from cosmpy.protos.cosmwasm.wasm.v1.tx_pb2 import MsgExecuteContract
+from evmospy.evmosproto.ethermint.evm.v1.tx_pb2 import MsgEthereumTx, LegacyTx
 
 from src.decoder.decoder import Decoder
 from src.contract import Contract 
 
 
 @dataclass
-class CosmWasmDecoder(Decoder):
+class EVMDecoder(Decoder):
     """ CosmWasm VM implementation of the Decoder class.
         Currently works for Juno and Terra 2.
     """
-    relevant_type_url: str = "/cosmwasm.wasm.v1.MsgExecuteContract"
+    relevant_type_url: str = "/ethermint.evm.v1.MsgEthereumTx"
 
     @staticmethod
     def decode_tx(tx: str) -> tuple[bytes, Tx]:
@@ -25,23 +26,19 @@ class CosmWasmDecoder(Decoder):
         return tx_bytes, Tx().FromString(tx_bytes)
 
     @staticmethod
-    def decode_message(message) -> tuple[MsgExecuteContract, dict]:
+    def decode_message(message) -> tuple[MsgExecuteContract, LegacyTx]:
         """ This method is used to decode a message,
             and return the message value and the decoded msg.
         """
-        message_value: MsgExecuteContract = MsgExecuteContract().FromString(message.value)
-        return message_value, json.loads(message_value.msg.decode("utf-8"))
+        message_value: MsgEthereumTx = MsgEthereumTx().FromString(message.value)
+        native_eth_tx: LegacyTx = LegacyTx().FromString(message_value.data.value)
+        return message_value, native_eth_tx
 
     @staticmethod
     def get_relevant_contract(contracts: dict, 
                               message_value, 
-                              msg) -> Contract or None:
+                              msg: LegacyTx) -> Contract or None:
         """ This method is used to get the relevant contract
             from a message, if it exists.
         """
-        contract: Contract = contracts.get(message_value.contract)
-        if contract is not None:
-            return contract
-        if "send" in msg and "contract" in msg["send"]:
-            return contracts.get(msg["send"]["contract"])
-        return None
+        return contracts.get(msg.to)
