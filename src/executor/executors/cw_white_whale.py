52239f1f3d99400059863b10a6d2fce753710490
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-01-23 01:44:46 -0500

    🔥 Remove function placeholders

--- src/executor/executors/cw_white_whale.py
+++ src/executor/executors/cw_white_whale.py
@@ -2,10 +2,5 @@ from src.executor import Executor
 
 class WhiteWhaleExecutor(Executor):
     
-    @staticmethod
-    def build_most_profitable_bundle():
-        pass
-    
-    @staticmethod
     def build_backrun_tx():
         pass
